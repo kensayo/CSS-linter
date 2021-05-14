1bceb3866d8ff25da5985c6a9746d2811be7b499
Kenny Ortega <kennyaoc@gmail.com>
2021-05-14 14:27:25 -0400

    Completed tests for errors class

--- spec/errors_spec.rb
+++ spec/errors_spec.rb
@@ -1,13 +1,57 @@
-require_relative '../lib/files'
+require_relative '../lib/errors'
 
 RSpec.describe '/lib/errors.rb' do
   let(:error) { Errors.new('../CSS-linter/test_files/file_1.css') }
+  let(:line1) { 'body {' }
+  let(:line2) { '  margin: 20px;' }
+  let(:line3) { '#my_id {' }
+  let(:line4) { '.my-class a {' }
+  let(:line5) { 'class-error{' }
+  let(:line6) { '} ' }
+  let(:line7) { 'margin: 20px;' }
+  let(:line8) { '  border-top: 20px' }
+
   context 'When Errors object is created' do
     it 'on #initalize' do
       expect(error.file_name).to eq('../CSS-linter/test_files/file_1.css')
     end
   end
-  it 'on #error_check' do
-    expect()
+  describe '#error_check' do
+    context 'When there are no errors in CSS' do
+      it 'in Class name' do
+        error.error_check(line1, 0)
+        expect(error.error.empty?).to be(true)
+      end
+      it 'on Assignation' do
+        error.error_check(line2, 0)
+        expect(error.error.empty?).to be(true)
+      end
+      it 'in ID Name' do
+        error.error_check(line3, 0)
+        expect(error.error.empty?).to be(true)
+      end
+      it 'on Class and Element' do
+        error.error_check(line4, 0)
+        expect(error.error.empty?).to be(true)
+      end
+    end
+    context 'When there are errors in CSS' do
+      it 'in Class name' do
+        error.error_check(line5, 0)
+        expect(error.error.empty?).to be(false)
+      end
+      it 'on Assignation' do
+        error.error_check(line6, 0)
+        expect(error.error.empty?).to be(false)
+      end
+      it 'in ID Name' do
+        error.error_check(line7, 0)
+        expect(error.error.empty?).to be(false)
+      end
+      it 'on Class and Element' do
+        error.error_check(line8, 0)
+        expect(error.error.empty?).to be(false)
+      end
+    end
   end
 end
